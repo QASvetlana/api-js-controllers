24ff667fc4ad39d75862358cd95be0c93c081957
Sveta Andreeva <89111378147v@yandex.com>
2023-09-27 00:00:53 +0300

    add first test for bookstore api

---
+++ specs/config.js
@@ -0,0 +1,14 @@
+//сюда выносим данные, которые будем использовать
+const config = {
+    url: 'https://bookstore.demoqa.com', // base url
+    credentials: {
+        userName: 'eveholt',
+        password: 'cityslicka'
+}
+}
+export default config; // делаем возможность экспорта
+
+/*
+process.env/API_USERNAME
+process.env/API_PASSWORD
+*/
---
+++ specs/user/index.spec.js
@@ -0,0 +1,46 @@
+import supertest from "supertest";
+import user from "../helper/user";
+import config from "../config";
+import { async } from "regenerator-runtime";
+
+//describe('user', () => {
+  describe('POST /Account/AccountV1AuthorizedPost', () => {
+    test('Метод должен существовать', async () => {
+      const res = await supertest('https://bookstore.demoqa.com')
+          .post('/Account/AccountV1AuthorizedPost')
+          .send({})
+
+      expect(res.status).not.toEqual(404);
+    })
+
+  /*  test('Авторизация должна проходить успешно с правильным логином и паролем', async () => {
+      const res = await user.login(config.credentials)
+
+      expect(res.status).toEqual(200);
+      expect(typeof res.body.token).toEqual('string')
+    })
+
+    test('Авторизация должна возвращать статус с кодом ошибки если логин неверный', async () => {
+      const res = await user.login({username: 'demo4', password: 'demo'})
+
+      expect(res.status).toEqual(412);
+      expect(res.body.code).toEqual(1011)
+    })
+
+    test('Авторизация должна возвращать статус с кодом ошибки если пароль неверный', async () => {
+      const res = await user.login({username: 'demo', password: 'demo3'})
+
+      expect(res.status).toEqual(412);
+      expect(res.body.code).toEqual(1011)
+    })
+  })
+
+  describe('GET /api/v1/user', () => {
+    test('Получение информации о пользователе', async() => {
+      const token = await user.getAuthToken();
+      // т.к. токен был получен ранее, можно к нему обратиться
+      const res = await user.user(token);
+      expect(res.status).toEqual(200);
+    })*/
+  })
+  
